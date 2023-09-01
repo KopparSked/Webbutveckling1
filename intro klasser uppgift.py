15011751dde0dc92866db7914d8ac2a2514f13d8
Albin <albin.kopparhed@gmail.com>
2023-09-01 11:03:49 +0200

    forgot to add while true

--- intro klasser uppgift.py
+++ intro klasser uppgift.py
@@ -16,22 +16,23 @@ class teknikare:
         return return_string
 
 def visa_namn_eller_kör(teknikarna):
-    val = input("Välj ett alternativ:\n1. Visa namnen\n5. Kör programmet som vanligt\n")
+    while True:
+        val = input("Välj ett alternativ:\n1. Visa namnen\n5. Kör programmet som vanligt\n")
 
-    if val == "1":
-        for namn in Namn:
-            print(namn)
-    elif val == "2":
-        for teknikare in teknikarna:
-            print(teknikare)
-            print("Oj du va så kattig niklas att", random.choice(Namn), "fick en riktigt hård dari")
-    else:
+        if val == "1":
+            for namn in Namn:
+                print(namn)
+        elif val == "2":
+            for teknikare in teknikarna:
+                print(teknikare)
+                print("Oj du va så kattig niklas att", random.choice(Namn), "fick en riktigt hård dari")
+        else:
             print("Oj, nu blev det fel här. Är du dum eller jag skrev ju hur man väljer och du skriver nåt annat jävla mongo.")
 
-    fortsätt = input("Vill du fortsätta? (ja/nej): ")
-    if fortsätt.lower() != "ja":
-        breakpoint
-        
+        fortsätt = input("Vill du fortsätta? (ja/nej): ")
+        if fortsätt.lower() != "ja":
+            breakpoint
+
 hård_dari = random.randrange(20)
 
 if __name__ == "__main__":
